5cb0b500ceb94601b732ec1a6f6a19b7ed1de403
champix56 <desorbaix@free.fr>
2023-03-27 16:02:51 +0200

    creation d'un composant basique

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import React from 'react';
 import {SafeAreaView, useColorScheme} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Button from './components/uis/Button/Button';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -17,7 +18,11 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  return <SafeAreaView style={backgroundStyle}></SafeAreaView>;
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Button />
+    </SafeAreaView>
+  );
 }
 
 // const styles = StyleSheet.create({});
---
+++ src/components/uis/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Button = () => {
+  return (
+    <View>
+      <Text>Demat Breizh</Text>
+    </View>
+  );
+};
+
+export default Button;
