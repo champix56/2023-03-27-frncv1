3053eb96f5c217514fb813de0a134bd0e4c9df89
champix56 <desorbaix@free.fr>
2023-03-28 10:24:03 +0200

    meo d'une ternaire

--- src/components/uis/Button/Button.js
+++ src/components/uis/Button/Button.js
@@ -3,22 +3,25 @@ import PropTypes from 'prop-types';
 import {Text, TouchableHighlight} from 'react-native';
 import styles from './Button.styles';
 const Button = props => {
-  //console.log(props);
+  console.log(props);
   return (
     <TouchableHighlight
       style={{...styles.Button, ...props.style, backgroundColor: props.bgcolor}}
       onPress={evt => {
         console.log(evt.target);
       }}>
-      <Text style={{...styles.text, color: props.color}}>{props.text}</Text>
+      {props.text ? (
+        <Text style={{...styles.text, color: props.color}}>{props.text}</Text>
+      ) : null}
     </TouchableHighlight>
   );
 };
 Button.propTypes = {
   bgcolor: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  text: PropTypes.string,
   style: PropTypes.object,
+  children: PropTypes.any,
 };
 Button.defaultProps = {
   bgcolor: 'skyblue',
