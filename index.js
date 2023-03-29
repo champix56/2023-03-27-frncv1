f52bd014ea92235a2ffd8c88d275fa3c70e4b8bf
champix56 <desorbaix@free.fr>
2023-03-29 12:35:31 +0200

    gestion context Provider pour react-redux

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import React from 'react';
+import {Provider} from 'react-redux';
+import {store} from './src/store/store';
+
+const ProviderWrapper = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => ProviderWrapper);
