d838b84741f1357a6f15d7aefca4e3ecc3e05d01
champix56 <desorbaix@free.fr>
2023-03-29 17:03:19 +0200

    gestion du binding avec state local

--- src/pages/EditProduct/EditProduct.js
+++ src/pages/EditProduct/EditProduct.js
@@ -13,37 +13,78 @@ import Button from '../../components/uis/Button/Button';
 /**
  * initial value of editProductState
  */
-const editProductInitialStateValue = '';
+const editProductInitialStateValue = {
+  name: '',
+  description: '',
+  prix: 0,
+  stock: 0,
+  image:
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png',
+};
 /**
  * EditProduct component
  * @param {object} props EditProduct props of component
  * @returns render of EditProduct component
  */
 const EditProduct = props => {
-  const [editProductState, seteditProductState] = useState(
+  const [produitState, setProduitState] = useState(
     editProductInitialStateValue,
   );
-
+  useEffect(() => {
+    setProduitState(props.produit);
+  }, [props.produit]);
   return (
     <>
       <View style={styles.EditProduct}>
         <Text style={{...styles.pageTitle, ...styles.centerText}}>
           Edition produit
         </Text>
-        <Text style={styles.centerText}>id:XX</Text>
+        <Text style={styles.centerText}>
+          {undefined !== produitState.id
+            ? `id:${produitState.id}`
+            : 'Nouveau produit'}
+        </Text>
         <View style={styles.mainContainer}>
           <View style={styles.leftCol}>
             <Text style={styles.valueHeader}>Titre</Text>
-            <TextInput style={styles.valueInput} placeholder="saisir ici" />
+            <TextInput
+              style={styles.valueInput}
+              placeholder="saisir ici"
+              value={produitState.name}
+              onChangeText={textInputValue => {
+                setProduitState({...produitState, name: textInputValue});
+              }}
+            />
             <Text style={styles.valueHeader}>Prix</Text>
-            <TextInput style={styles.valueInput} placeholder="saisir ici" />
+            <TextInput
+              style={styles.valueInput}
+              placeholder="saisir ici"
+              value={produitState.prix.toFixed(2)}
+              onChangeText={textInputValue => {
+                setProduitState({
+                  ...produitState,
+                  prix: Number(textInputValue),
+                });
+              }}
+            />
             <Text style={styles.valueHeader}>Photo</Text>
-            <TextInput style={styles.valueInput} placeholder="saisir ici" />
+            <TextInput
+              style={styles.valueInput}
+              placeholder="saisir ici"
+              value={produitState.image}
+              onChangeText={textInputValue => {
+                setProduitState({...produitState, image: textInputValue});
+              }}
+            />
             <Text style={styles.valueHeader}>Description</Text>
             <TextInput
               style={{...styles.valueInput, ...styles.multilineInput}}
               placeholder="saisir ici"
               multiline={true}
+              value={produitState.description}
+              onChangeText={textInputValue => {
+                setProduitState({...produitState, description: textInputValue});
+              }}
             />
           </View>
           <View style={styles.rightCol}>
@@ -52,7 +93,7 @@ const EditProduct = props => {
               resizeMode="center"
               style={styles.image}
               source={{
-                uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png',
+                uri: produitState.image,
               }}
             />
           </View>
@@ -78,9 +119,15 @@ const styles = StyleSheet.create({
     fontSize: 19,
     fontWeight: '700',
   },
-  valueInput: {fontSize: 17, backgroundColor: '#ACACAC', width: '100%'},
+  valueInput: {
+    fontSize: 17,
+    backgroundColor: '#ACACAC',
+    width: (Dimensions.get('window').width / 3) * 2,
+  },
   multilineInput: {
     minHeight: 150,
+    maxHeight: 500,
+    verticalAlign: 'top',
   },
   leftCol: {flexGrow: 1},
   rightCol: {width: Dimensions.get('window').width / 3},
@@ -88,7 +135,17 @@ const styles = StyleSheet.create({
   centerText: {textAlign: 'center'},
   image: {width: Dimensions.get('window').width / 3, minHeight: 64},
 });
-EditProduct.propTypes = {};
+EditProduct.propTypes = {
+  produit: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string.isRequired,
+    prix: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    stock: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+  saveProduct: PropTypes.func.isRequired,
+};
 EditProduct.defaultProps = {};
 
 export default EditProduct;
